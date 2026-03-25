acad600468a44cbf59c2c51f90bf3feb5f8fe44b
Daniel Sepulveda Chiguils <daniel.sepulveda@iknesoft.com>
2026-03-24 18:15:53 -0600

    Agregar modelo para Hogares (HouseHold) con migración, factory, seeder y controlador

---
+++ app/Models/Household.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Household extends Model
+{
+    /** @use HasFactory<\Database\Factories\HouseholdFactory> */
+    use HasFactory;
+
+    public const UPDATED_AT = null;
+
+    protected $fillable = [
+        'name',
+        'invitation_code',
+        'created_by'
+    ];
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+}
